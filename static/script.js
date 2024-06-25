5f47cfb2de75859935026cdc306ed1ed4246755e
Pedro Nunes <167380405+devnunes7@users.noreply.github.com>
2024-06-25 00:15:43 -0400

    sobre e habilidades

--- static/script.js
+++ static/script.js
@@ -9,3 +9,18 @@ menuMobile.classList.contains ("bi-list")
 :menuMobile.classList.replace ("bi-x", "bi-list");
 body.classList.toggle("menu-nav-active");
 })
+
+
+/* fecha o menu quando clicar em algum item e muda o icone para list */
+
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item => {
+  item.addEventListener ("click", () =>{
+    if (body.classList.contains ("menu-nav-active")) {
+    body.classList.remove("menu-nav-active") 
+    menuMobile.classList.replace("bi-x", "bi-list")
+    }
+  })
+})
